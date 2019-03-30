a81511b352005b1f117e08fe800c89dc19c09466
Lauzzanne <49020477+Lauzzanne@users.noreply.github.com>
2019-03-30 20:54:56 +0900

    Create Practice.java
    if/else if/else practice

---
+++ Practice.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("기상 시간: ");
+        System.out.println("피로도: ");
+
+        int time = scan.nextInt();
+        int tiredness = scan.nextInt();
+        if (time <= 7 && tiredness < 5) {
+            System.out.println("아침먹고 운동하기");
+        } else if (time <= 8 && tiredness > 3 && tiredness <= 7) {
+            System.out.println("시리얼 먹고 운동하기");
+        } else {
+            System.out.println("시리얼이라도 먹자");
+        }
+    }
+}
